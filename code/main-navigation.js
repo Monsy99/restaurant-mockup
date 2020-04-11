7f1b55e5202dc421f1d8d8015f4c4c59dbe1b4eb
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-04-11 20:23:59 +0200

    js to menu on small devices and main nav changes

---
+++ code/main-navigation.js
@@ -0,0 +1,12 @@
+(() => {
+    const button = document.querySelector(".js-main-navigation__mobile-button");
+    const menu = document.querySelector(".js-main-navigation__menu");
+    const toggleClass = () => {
+        menu.classList.toggle("main-navigation__menu--open");
+    };
+    if (button) {
+        button.addEventListener("click", toggleClass);
+        menu.addEventListener("click", toggleClass);
+    }
+
+})();
