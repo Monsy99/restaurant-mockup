e98bd89c34bd88ad6ce6630cb18a79de8fa88d4b
Szymon Marczuk <szymon.marczukk@gmail.com>
2020-12-07 23:53:49 +0100

    added a scroll behavior smooth (not working in chrome)

--- code/main-navigation.js
+++ code/main-navigation.js
@@ -1,12 +1,11 @@
 (() => {
-    const button = document.querySelector(".js-main-navigation__mobile-button");
-    const menu = document.querySelector(".js-main-navigation__menu");
-    const toggleClass = () => {
-        menu.classList.toggle("main-navigation__menu--open");
-    };
-    if (button) {
-        button.addEventListener("click", toggleClass);
-        menu.addEventListener("click", toggleClass);
-    }
-
+  const button = document.querySelector(".js-main-navigation__mobile-button");
+  const menu = document.querySelector(".js-main-navigation__menu");
+  const toggleClass = () => {
+    menu.classList.toggle("main-navigation__menu--open");
+  };
+  if (button) {
+    button.addEventListener("click", toggleClass);
+    menu.addEventListener("click", toggleClass);
+  }
 })();
